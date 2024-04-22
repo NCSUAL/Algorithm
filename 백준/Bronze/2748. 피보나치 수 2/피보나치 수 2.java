2eabe3256114394c89554d6a2c314a5fe3642b6a
LSH <141344623+NCSUAL@users.noreply.github.com>
2024-04-22 23:08:56 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 112 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long[] fib = new long[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+}
